f433ffc6f90e09848d6671bc4b461bf497e2e487
roldanx <sergio.roldan@ibm.com>
2022-04-12 20:23:25 +0200

    Redesign app to handle several models

--- app/app.py
+++ app/app.py
@@ -7,8 +7,7 @@ import os
 import yaml
 
 registry_uri = os.getenv('REGISTRY_URI')
-mlruns_directory = os.getenv('MLRUNS_DIRECTORY')
-model_location= os.path.join(mlruns_directory, '1/d07c2eed5f494aeebcb396fe8af89d46/artifactsX')
+# UNUSED: mlruns_directory = os.getenv('MLRUNS_DIRECTORY')
 template_dir='./templates'
 
 app = Flask(__name__,template_folder=template_dir)
@@ -29,39 +28,77 @@ def validate_input(dict_request):
             raise NotANumber
     return True
 
-def predict(data):
-    model = mlflow.sklearn.load_model(model_location) # --> folder required
-    # model = joblib.load(model_location) --> .pkl file required
-    # model = pickle.load(open(model_location, 'rb')) --> .pkl file required
+def get_model_path(model_name):
+    client = mlflow.tracking.MlflowClient(registry_uri=registry_uri)
+    model_path = client.get_latest_versions(name=model_name, stages=['Production'])
+    if model_path:
+        print("Prod loaded")
+        return model_path[0].source
+    model_path = client.get_latest_versions(name=model_name, stages=['Staging'])
+    if model_path:
+        print("Staging loaded")
+        return model_path[0].source
+    model_path = client.get_latest_versions(name=model_name, stages=['None'])
+    if model_path:
+        print("Unclassified model loaded")
+        return model_path[0].source
+    else:
+        print("No model named '" + model_name + "' found on this MLFlow server")
+
+def predict(data, model_path):
+    model = mlflow.sklearn.load_model(model_path) # --> folder required
+    # model = joblib.load(model_path) --> .pkl file required
+    # model = pickle.load(open(model_path, 'rb')) --> .pkl file required
     prediction = model.predict(data).tolist()[0]
     return prediction
 
-def form_response(dict_request):
+def form_response(dict_request, model_name):
     try:
         if validate_input(dict_request):
             data = dict_request.values()
             data = [list(map(float, data))]
-            response = predict(data)
+            model_path = get_model_path(model_name)
+            response = predict(data, model_path)
             return response
     except NotANumber as e:
         response =  str(e)
         return response 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
+    return render_template('index.html')    
+    
+@app.route('/tree', methods=['GET', 'POST'])
+def tree():
     if request.method == 'POST':
         try:
             if request.form:
                 dict_req = dict(request.form)
-                response = form_response(dict_req)
-                return render_template('index.html', response=response)
+                response = form_response(dict_req, "tree_model")
+                return render_template('tree_model.html', response=response)
         except Exception as e:
             print(e)
             error = {'error': "Something went wrong!! Try again later!"}
             error = {'error': e}
             return render_template('404.html', error=error)
     else:
-        return render_template('index.html')
-
+        return render_template('tree_model.html')
+    
+@app.route('/nn', methods=['GET', 'POST'])
+def nn():
+    if request.method == 'POST':
+        try:
+            if request.form:
+                dict_req = dict(request.form)
+                response = form_response(dict_req, "neural_network")
+                return render_template('nn_model.html', response=response)
+        except Exception as e:
+            print(e)
+            error = {'error': "Something went wrong!! Try again later!"}
+            error = {'error': e}
+            return render_template('404.html', error=error)
+    else:
+        return render_template('nn_model.html')
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
